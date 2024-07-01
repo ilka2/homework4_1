64ff4d5c4619d9e335a7b0fbed683fcd41c6accb
Ilya <kravtsov-ilya@mail.ru>
2024-07-01 19:03:34 +0300

    Добавьте файлы проекта.

---
+++ homework4_1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Figure
+{
+    string name;
+    int side;
+
+public:
+
+    Figure(string name = "Фигура", int side = 0)
+    {
+        this->name = name;
+        this->side = side;
+    }
+
+    void Information()
+    {
+        cout << "Число сторон у \"" << name << "\": " << side << endl;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    Triangle() :Figure("Треугольник", 3)
+    {
+
+    }
+};
+
+class RRectangle : public Figure
+{
+public:
+    RRectangle() :Figure("Прямоугльник", 4)
+    {
+
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Figure figure;
+    Triangle triangle;
+    RRectangle rectangle;
+
+    figure.Information();
+    triangle.Information();
+    rectangle.Information();
+}
